4e11eb605912200de05c30c2eb587ab0129a074d
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2009-12-10 17:17:44 +0100

    roby: fix merging in the context of the new data source structure
    The problem is to recognize the mergeable tasks, and then to
    perform the merge. The following convention is now used:
     - A is merged into B only if B has a data source that is free
       (not assigned to any concrete source through the corresponding
       source_name argument) or a source that is assigned to the same
       source name.
     - ports are mapped for subsources. In order to represent that,
       a port "bla" defined in the source's interface is mapped to
       "slave_sourceBla" on the slave source. Note that the remapping
       algorithm itself is not implemented and trying to merge this
       way will lead to a NotImplementedError

---
+++ test/data/system/DataTypes.hpp
@@ -0,0 +1,40 @@
+#ifndef SYSTEM_DATATYPES_HPP
+#define SYSTEM_DATATYPES_HPP
+
+namespace base {
+    struct PointCloud3D {
+        int dummy;
+    };
+}
+
+namespace can {
+    struct Message {
+        int dummy;
+    };
+}
+
+namespace control {
+    struct MotorsCommand {
+        int dummy;
+    };
+    struct MotorsState {
+        int dummy;
+    };
+    struct MotionCommand {
+        int dummy;
+    };
+}
+
+namespace camera {
+    struct Image {
+        int dummy;
+    };
+}
+namespace imu {
+    struct Orientation {
+        int dummy;
+    };
+}
+
+#endif
+
