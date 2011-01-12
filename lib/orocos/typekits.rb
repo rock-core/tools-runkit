439b69e426e8de9c7adcd294b63413dda5519c05
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2011-01-12 13:46:00 +0100

    export the main type registry into the Types module by default
    This can be disabled by setting Orocos.export_types to false

--- lib/orocos/typekits.rb
+++ lib/orocos/typekits.rb
@@ -1,3 +1,6 @@
+module Types
+end
+
 module Orocos
     class << self
         # The set of typekits that are loaded in this Ruby instance, as an
@@ -12,9 +15,23 @@ module Orocos
         def loaded_typekit?(name)
             loaded_typekits.include?(name)
         end
+
+        # If true, the types that get loaded are exported in the Ruby namespace.
+        # For instance, a /base/Pose type included in Orocos.registry will be
+        # available as Base::Pose
+        #
+        # The export can be done in a sub-namespace by setting
+        # Orocos.type_export_namespace
+        attr_predicate :export_types?
+
+        # The namespace in which the types should be exported if
+        # Orocos.export_types? is true. It defaults to Types
+        attr_accessor :type_export_namespace
     end
     @loaded_typekits = []
     @loaded_plugins = Set.new
+    @export_types = true
+    @type_export_namespace = Types
 
     # Generic loading of a RTT plugin
     def self.load_plugin_library(pkg, name, libname) # :nodoc:
@@ -68,11 +85,37 @@ module Orocos
         # data types. orogen defines a type_registry field in the pkg-config
         # file for that purpose.
         tlb = typekit_pkg.type_registry
-        if tlb
-            Orocos.registry.import(tlb)
+        if tlb # this is an orogen typekit
+            begin
+                Orocos.master_project.using_typekit(name)
+                Orocos.registry.import(tlb)
+            rescue RuntimeError => e
+                raise e, "failed to load typekit #{name}: #{e.message}", e.backtrace
+            end
+
+            if Orocos.export_types?
+                Orocos.registry.export_to_ruby(Orocos.type_export_namespace) do |name, base_type, mod, basename, exported_type|
+                    if name =~ /orogen_typekits/ # just ignore those
+                    elsif base_type <= Typelib::NumericType # using numeric is transparent in Typelib/Ruby
+                    elsif base_type.contains_opaques? # register the intermediate instead
+                        Orocos.master_project.intermediate_type_for(base_type)
+                    elsif Orocos.master_project.m_type?(base_type) # just ignore, they are registered as the opaque
+                    else exported_type
+                    end
+                end
+            end
         end
 
         nil
+    end
+
+    # Loads all typekits that are available on this system
+    def self.load_all_typekits
+        Orocos.available_projects.each_key do |project_name|
+            if master_project.has_typekit?(project_name)
+                load_typekit(project_name)
+            end
+        end
     end
 end
 
