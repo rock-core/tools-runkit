rock-core/tools-runkit

commit 6071fd44097275e1e27bbfe21b31f98006483393

roby: #setup resets exceptions, remove the handling in update_state_change

lib/orocos/roby/task_context.rb
@@ -737,11 +737,6 @@ module Orocos
                 if orogen_task.exception_state?(orogen_state)
                     @stopping_because_of_error = true
                     @stopping_origin = orogen_state
-		    begin
-		        orogen_task.reset_exception(false)
-		    rescue Orocos::StateTransitionFailed => e
-			Robot.warn "cannot reset error on #{name}: #{e.message}"
-		    end
                 elsif orogen_task.fatal_error_state?(orogen_state)
                     if event = state_event(orogen_state)
                         emit event

commit 06bdfd6753996fec0ccb4eb26915999589717f8a

roby: fix event emissions related to exception states
This had been broken in 6071fd44097275e1e27bbfe21b31f98006483393,
where the call to reset_exceptions has been removed.

Beforehand, reset_exception was causing a transition to STOPPED
or PRE_OPERATIONAL, which was handled in a different part of
handle_state_changes. Removing that, we need to emit the exception
event directly.

lib/orocos/roby/task_context.rb
@@ -739,8 +739,10 @@ module Orocos
                 end
 
                 if orogen_task.exception_state?(orogen_state)
-                    @stopping_because_of_error = true
-                    @stopping_origin = orogen_state
+                    if event = state_event(orogen_state)
+                        emit event
+                    else emit :exception
+                    end
                 elsif orogen_task.fatal_error_state?(orogen_state)
                     if event = state_event(orogen_state)
                         emit event
@@ -751,13 +753,7 @@ module Orocos
                     emit :running
 
                 elsif orogen_state == :STOPPED || orogen_state == :PRE_OPERATIONAL
-                    if @stopping_because_of_error
-                        if event = state_event(@stopping_origin)
-                            emit event
-                        else
-                            emit :failed
-                        end
-                    elsif interrupt?
+                    if interrupt?
                         emit :interrupt
                     elsif finishing?
                         emit :stop