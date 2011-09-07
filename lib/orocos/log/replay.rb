617015776a22f80fea1deda4e3f15931a4fb0a3b
Alexander Duda <Alexander.Duda@dfki.de>
2011-09-07 15:47:18 +0200

    replay: add filter options
    someone can now call replay.track(true,{:tasks=> /Name/}...)
    to select ports which should be replayed even if they are not connected.
    
    Allowed filter options are:
    :tasks, :types; :ports; :limit
    
    if a port has more samples than limit the port is ignored

--- lib/orocos/log/replay.rb
+++ lib/orocos/log/replay.rb
@@ -544,7 +544,7 @@ module Orocos
                 return ports
             end
 
-            #Returns true if the task has used tasks
+            #Returns true if the task has used ports
             def used?
               !used_ports.empty?
             end
@@ -577,11 +577,23 @@ module Orocos
                 end
             end
 
-            #If set to true all ports are replayed
+            #If set to true all ports are replayed 
             #otherwise only ports are replayed which have a reader or
             #a connection to an other port
-            def track(value)
+            def track(value,filter = Hash.new)
+                options, filter = Kernel::filter_options(filter,[:ports,:types,:limit])
+                raise "Cannot understand filter: #{filter}" unless filter.empty?
+
                 @ports.each_value do |port|
+                    if(options.has_key? :ports)
+                        next unless port.name =~ options[:ports]
+                    end
+                    if(options.has_key? :types)
+                        next unless port.type_name =~ options[:types]
+                    end
+                    if(options.has_key? :limit)
+                        next unless port.number_of_samples <= options[:limit]
+                    end
                     port.tracked = value
                     puts "set" + port.stream.name + value.to_s
                 end
@@ -778,12 +790,14 @@ module Orocos
                 timestamps[type_name] = block
             end
 
-            #If set to true all ports are replayed
+            #If set to true all ports are replayed and are not filtered out by the
+            #filter
             #otherwise only ports are replayed which have a reader or
             #a connection to an other port
-            def track(value)
+            def track(value,filter=Hash.new)
+                options, filter = Kernel::filter_options(filter,[:tasks])
                 @tasks.each_value do |task|
-                    task.track(value)
+                    task.track(value,filter) if !options.has_key?(:tasks) || task.name =~ options[:tasks]
                 end
             end
 
