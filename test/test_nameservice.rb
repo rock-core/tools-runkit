1c2fb7c2eada16146dd28f39bd55e690b5913561
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2011-11-01 17:29:07 +0100

    make the nameservice test idempotent and include it in the complete test suite

--- test/test_nameservice.rb
+++ test/test_nameservice.rb
@@ -16,42 +16,46 @@ avahi_options = { :searchdomains => [ "_orocosrbtest._tcp" ] }
 describe "Nameservice" do
     include Orocos::Spec
 
+    def setup
+        Nameservice::reset
+    end
+
+    def teardown
+        # Reenable CORBA
+        Nameservice.reset
+        Nameservice.enable('CORBA')
+    end
+
     it "throws on enabling unknown type" do
         assert_raises(NameError) { Nameservice::Provider::get_instance_of("foo", {}) }
     end
 
     it "throws on resolve without providers" do
-        Nameservice::reset
         assert_raises(Orocos::NotFound) { Nameservice::resolve("foo") }
     end
 
     it "throws if resolve is not implemented" do
-        Nameservice::reset
         Nameservice::enable(:Provider)     
         assert_raises(NotImplementedError) { Nameservice::resolve("foo") }
     end
 
     it "throws if resolve_by_type is not supported" do
-        Nameservice::reset
         Nameservice::enable(:Provider)     
         assert(Nameservice::resolve_by_type("foo"))
     end
 
     it "throws if resolve fails for AVAHI" do
-        Nameservice::reset
         Nameservice::enable(:AVAHI, avahi_options)     
         assert_raises(Orocos::NotFound) { Nameservice::resolve("foo") }
     end
 
     it "retrieves nameservice instance when available" do
-        Nameservice::reset
         assert(!Nameservice::get(:AVAHI))
         Nameservice::enable(:AVAHI, avahi_options)     
         assert(Nameservice::get(:AVAHI))
     end
 
     it "retrieves options for available type" do
-        Nameservice::reset
         assert_raises(NameError) { Nameservice::options(:BLA) }
         Nameservice::enable(:AVAHI, avahi_options)
         options = Nameservice::options(:AVAHI)
@@ -59,14 +63,12 @@ describe "Nameservice" do
     end
 
     it "allows checking if enabled" do
-        Nameservice::reset
         assert(!Nameservice.enabled?(:AVAHI))
         Nameservice::enable(:AVAHI, avahi_options)
         assert(Nameservice::enabled?(:AVAHI))
     end
 
     it "check on required option in AVAHI" do
-        Nameservice::reset
         assert_raises(ArgumentError) { Nameservice::enable(:AVAHI) }
     end
         
