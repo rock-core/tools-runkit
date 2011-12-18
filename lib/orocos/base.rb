e3d3ba6dedf747e1305189504cb7bebb06880089
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2011-12-18 10:36:22 +0100

    in Orocos.load, un-export the last registry if we get called multiple times

--- lib/orocos/base.rb
+++ lib/orocos/base.rb
@@ -120,6 +120,9 @@ module Orocos
 
     def self.load
         @master_project = Orocos::Generation::Component.new
+        if registry && export_types?
+            registry.clear_exports(type_export_namespace)
+        end
         @registry = master_project.registry
         @conf = ConfigurationManager.new
         @available_projects ||= Hash.new
