2862cf0cdb218897ac654f3a57f22b90eb3a57d4
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2012-09-30 10:27:03 +0200

    rake: move the component-generation code out of orocos/test into orocos/rake
    The issue here was that we load flexmock/test_unit in orocos/test, which
    was leading to trying to run test suites when we do rake setup:orogen_*

---
+++ lib/orocos/rake.rb
@@ -0,0 +1,56 @@
+require 'orogen'
+module Orocos
+    module Rake
+        USE_MQUEUE =
+            if ENV['USE_MQUEUE'] == '1'
+                puts "MQueue enabled through the USE_MQUEUE environment variable"
+                puts "set USE_MQUEUE=0 to disable"
+                true
+            else
+                puts "use of MQueue disabled. Set USE_MQUEUE=1 to enable"
+                false
+            end
+
+        # Generates, builds and installs the orogen component defined by the
+        # orogen description file +src+. The compiled package is installed in
+        # +prefix+
+        def self.generate_and_build(src, work_basedir)
+            src_dir  = File.dirname(src)
+            src_name = File.basename(src_dir)
+
+            FileUtils.mkdir_p work_basedir
+            work_dir = File.join(work_basedir, src_name)
+            if (ENV['TEST_KEEP_WC'] != "1") || !File.directory?(work_dir)
+                FileUtils.rm_rf work_dir
+                FileUtils.cp_r  src_dir, work_dir
+            end
+
+            prefix   = File.join(work_basedir, "prefix")
+            ruby_bin   = RbConfig::CONFIG['RUBY_INSTALL_NAME']
+            orogen_bin = File.expand_path('../bin/orogen', Orocos::Generation.base_dir)
+            Dir.chdir(work_dir) do
+                transports = %w{corba typelib}
+                if USE_MQUEUE
+                    transports << 'mqueue'
+                end
+
+                if !system(ruby_bin, orogen_bin, '--corba', '--no-rtt-scripting', "--transports=#{transports.join(",")}", File.basename(src))
+                    raise "failed to build #{src} in #{work_basedir}"
+                end
+
+                if !File.directory? 'build'
+                    FileUtils.mkdir 'build'
+                end
+                Dir.chdir 'build' do
+                    if !system 'cmake', "-DCMAKE_INSTALL_PREFIX=#{prefix}", "-DCMAKE_BUILD_TYPE=Debug", ".."
+                        raise "failed to configure"
+                    elsif !system "make", "install"
+                        raise "failed to install"
+                    end
+                end
+            end
+            ENV['PKG_CONFIG_PATH'] += ":#{prefix}/lib/pkgconfig"
+        end
+    end
+end
+
--- lib/orocos/test.rb
+++ lib/orocos/test.rb
@@ -2,6 +2,7 @@ require 'fileutils'
 require 'typelib'
 require 'orogen'
 require 'flexmock/test_unit'
+require 'orocos/rake'
 
 module Orocos
     OROCOS_TEST_MODES = (ENV['OROCOS_TEST_MODES'] || "").split(',')
@@ -47,16 +48,7 @@ module Orocos
 
         include Mocks
 
-        USE_MQUEUE =
-            if ENV['USE_MQUEUE'] == '1'
-                puts "MQueue enabled through the USE_MQUEUE environment variable"
-                puts "set USE_MQUEUE=0 to disable"
-                true
-            else
-                puts "use of MQueue disabled. Set USE_MQUEUE=1 to enable"
-                false
-            end
-
+        USE_MQUEUE = Orocos::Rake::USE_MQUEUE
 
         attr_reader :processes
 
@@ -74,47 +66,6 @@ module Orocos
             processes.clear
             super if defined? super
             Orocos.clear
-        end
-
-        # Generates, builds and installs the orogen component defined by the
-        # orogen description file +src+. The compiled package is installed in
-        # +prefix+
-        def self.generate_and_build(src, work_basedir)
-            src_dir  = File.dirname(src)
-            src_name = File.basename(src_dir)
-
-            FileUtils.mkdir_p work_basedir
-            work_dir = File.join(work_basedir, src_name)
-            if (ENV['TEST_KEEP_WC'] != "1") || !File.directory?(work_dir)
-                FileUtils.rm_rf work_dir
-                FileUtils.cp_r  src_dir, work_dir
-            end
-
-            prefix   = File.join(work_basedir, "prefix")
-            ruby_bin   = RbConfig::CONFIG['RUBY_INSTALL_NAME']
-            orogen_bin = File.expand_path('../bin/orogen', Orocos::Generation.base_dir)
-            Dir.chdir(work_dir) do
-                transports = %w{corba typelib}
-                if Test::USE_MQUEUE
-                    transports << mqueue
-                end
-
-                if !system(ruby_bin, orogen_bin, '--corba', '--no-rtt-scripting', "--transports=#{transports.join(",")}", File.basename(src))
-                    raise "failed to build #{src} in #{work_basedir}"
-                end
-
-                if !File.directory? 'build'
-                    FileUtils.mkdir 'build'
-                end
-                Dir.chdir 'build' do
-                    if !system 'cmake', "-DCMAKE_INSTALL_PREFIX=#{prefix}", "-DCMAKE_BUILD_TYPE=Debug", ".."
-                        raise "failed to configure"
-                    elsif !system "make", "install"
-                        raise "failed to install"
-                    end
-                end
-            end
-            ENV['PKG_CONFIG_PATH'] += ":#{prefix}/lib/pkgconfig"
         end
 
         def spawn_and_get(component, task = component)
