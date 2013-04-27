43e9d087651d0cdad009840d6c80fe45e67ec7cb
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2013-04-27 22:43:33 +0200

    test: add some tests that were missing in the suite* files

--- test/suite_async.rb
+++ test/suite_async.rb
@@ -2,10 +2,10 @@ require './test_helper'
 start_simple_cov("suite")
 
 ENV['ORO_LOGLEVEL'] = '3'
-require './async/test_object'
-require './async/test_task'
-require './async/test_ports'
 require './async/test_attributes'
-require './async/test_task_proxy'
 require './async/test_name_service'
+require './async/test_object'
+require './async/test_ports'
+require './async/test_task'
+require './async/test_task_proxy'
 
