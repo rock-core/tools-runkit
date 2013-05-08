6741b39f69fe9e64675467072c3866d3168ceb9c
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2013-05-08 16:52:03 +0200

    test,ros: test Node#initialize

--- test/ros/test_node.rb
+++ test/ros/test_node.rb
@@ -17,6 +17,33 @@ describe Orocos::ROS::Node do
         @name_service = Orocos::ROS::NameService.new
     end
 
+    describe "#initialize" do
+        before do
+            task = new_ruby_task_context 'ros_test'
+            port = task.create_input_port('out', '/base/Time')
+            port.subscribe_to_ros
+        end
+        it "should be running when accessed from the name service" do
+            node = name_service.get(Orocos::ROS.caller_id)
+            assert node.running?
+        end
+
+        it "should not be running when created from scratch" do
+            node = Orocos::ROS::Node.new(name_service, nil, "/mynode")
+            assert !node.running?
+        end
+
+        it "should be given an empty model" do
+            node = name_service.get(Orocos::ROS.caller_id)
+            assert node.model
+        end
+
+        it "should be given an absolute name" do
+            node = name_service.get(Orocos::ROS.caller_id)
+            assert_equal Orocos::ROS.caller_id, node.name
+        end
+    end
+
     it "should list its subscribed topics as input ports" do
         Orocos.load_typekit 'base'
         task = new_ruby_task_context 'ros_test'
