896d0c1fdf41f604c428ea3fff4dc8f0391e2fcf
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2013-11-20 09:49:18 +0100

    allow orogen to announce that some typekits are installed but disabled
    This can be used by typekits to install only the model information,
    without building anything. The generated ROS typekits work this
    way now.

--- lib/orocos/typekits.rb
+++ lib/orocos/typekits.rb
@@ -208,7 +208,12 @@ module Orocos
             AUTOLOADED_TRANSPORTS.each do |transport_name, required|
                 plugin_name = transport_library_name(name, transport_name, Orocos.orocos_target)
                 begin
-                    plugins[plugin_name] = [Utilrb::PkgConfig.new(plugin_name), required]
+                    pkg = Utilrb::PkgConfig.new(plugin_name)
+                    if pkg.disabled != "true"
+                        plugins[plugin_name] = [pkg, required]
+                    elsif required
+                        raise NotFound, "the '#{name}' typekit has a #{transport_name} transport installed, but it is disabled"
+                    end
                 rescue Utilrb::PkgConfig::NotFound => e
                     if required
                         raise NotFound, "the '#{name}' typekit has no #{transport_name} transport: could not find pkg-config package #{e.name} in #{ENV['PKG_CONFIG_PATH']}"
