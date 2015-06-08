rock-core/tools-runkit

commit da5be28ed1bc74efa7abfa61fe96c429845b3646

conf: fix string handling in normalize_value

lib/orocos/configurations.rb
@@ -370,6 +370,10 @@ module Orocos
         # @param [Typelib::Type] value_t the type we are validating against
         # @return [Object] a normalized configuration value
         def normalize_conf_value(value, value_t)
+            if value.respond_to?(:to_str)
+                return value
+            end
+
             case value
             when Typelib::ContainerType, Typelib::ArrayType
                 element_t = value_t.deference

test/test_configurations.rb
@@ -661,6 +661,13 @@ describe Orocos::TaskConfigurations do
             assert_kind_of type, result
             assert_equal 42, Typelib.to_ruby(result)
         end
+        it "converts typelib types that are valid string representations alone" do
+            compound_t = registry.create_compound('/S') { |c| c.add 'a', '/int' }
+            compound = compound_t.new(a: 10)
+            def compound.to_str; end
+            normalized = conf.normalize_conf_value(compound, compound_t)
+            assert_same normalized, compound
+        end
         it "converts typelib compound values to hashes" do
             compound_t = registry.create_compound('/S') { |c| c.add 'a', '/int' }
             compound = compound_t.new(a: 10)

commit c7e66bc9d3a91958b56662e7d2ae13c24082f766

conf: make handling of string values narrower
There are cases where e.g. a numerical value will be passed to
normalize_conf_value as strings (for instance when using unit
conversions). This was broken by da5be28ed1bc74efa7abfa61fe96c429845b3646
which would simply bypass the conversion completely.

Make the test narrower, and do run the conversion to typelib even
for string-like types.

lib/orocos/configurations.rb
@@ -370,8 +370,8 @@ module Orocos
         # @param [Typelib::Type] value_t the type we are validating against
         # @return [Object] a normalized configuration value
         def normalize_conf_value(value, value_t)
-            if value.respond_to?(:to_str)
-                return value
+            if value_t.method_defined?(:to_str)
+                return normalize_conf_terminal_value(value, value_t)
             end
 
             case value
@@ -389,24 +389,35 @@ module Orocos
             when Array
                 normalize_conf_array(value, value_t)
             else
-                begin
-                    if value_t <= Typelib::NumericType
-                        converted_value = evaluate_numeric_field(Typelib.to_ruby(value), value_t)
-                        typelib_value = Typelib.from_ruby(converted_value, value_t)
-                    else
-                        typelib_value = Typelib.from_ruby(value, value_t)
-                    end
-
-                    if typelib_value.class != value.class
-                        return normalize_conf_value(typelib_value, value_t)
-                    else
-                        typelib_value
-                    end
-
-                rescue ArgumentError => e
-                    raise ConversionFailed.new(e), e.message, e.backtrace
-                end
+                normalize_conf_terminal_value(value, value_t)
             end
+        end
+
+        # @api private
+        #
+        # Helper for {#normalize_conf_value} to normalize values that are
+        # terminal, i.e. that should be converted to a typelib value
+        def normalize_conf_terminal_value(value, value_t)
+            if value_t <= Typelib::NumericType
+                ruby_value = Typelib.to_ruby(value)
+                if ruby_value.respond_to?(:to_str)
+                    converted_value = evaluate_numeric_field(ruby_value, value_t)
+                else
+                    converted_value = value
+                end
+                typelib_value = Typelib.from_ruby(converted_value, value_t)
+            else
+                typelib_value = Typelib.from_ruby(value, value_t)
+            end
+
+            if typelib_value.class != value.class
+                return normalize_conf_value(typelib_value, value_t)
+            else
+                typelib_value
+            end
+
+        rescue ArgumentError => e
+            raise ConversionFailed.new(e), e.message, e.backtrace
         end
 
         # @api private

test/test_configurations.rb
@@ -661,12 +661,11 @@ describe Orocos::TaskConfigurations do
             assert_kind_of type, result
             assert_equal 42, Typelib.to_ruby(result)
         end
-        it "converts typelib types that are valid string representations alone" do
-            compound_t = registry.create_compound('/S') { |c| c.add 'a', '/int' }
-            compound = compound_t.new(a: 10)
-            def compound.to_str; end
-            normalized = conf.normalize_conf_value(compound, compound_t)
-            assert_same normalized, compound
+        it "converts /std/string as a final value instead of a container" do
+            string_t = registry.get '/std/string'
+            normalized = conf.normalize_conf_value("bla", string_t)
+            assert_kind_of string_t, normalized
+            assert_equal "bla", Typelib.to_ruby(normalized)
         end
         it "converts typelib compound values to hashes" do
             compound_t = registry.create_compound('/S') { |c| c.add 'a', '/int' }