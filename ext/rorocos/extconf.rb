700571a69461bc2627d5b7ef7ee55cc8f15a3edb
Thomas Roehr <thomas.roehr@dfki.de>
2016-06-28 14:05:47 +0200

    Fix extconf for debian packaging and allow installation of arch specific libraries

--- ext/rorocos/extconf.rb
+++ ext/rorocos/extconf.rb
@@ -5,9 +5,11 @@ ext_dir = "ext/rorocos"
 lib_dir = "lib/orocos"
 # Use absolute main package directory as starting point, since rake-compiler uses a build directory which depends on the system architecture and ruby version
 main_dir = File.join(File.dirname(__FILE__),"..","..")
-if prefix = ENV['CMAKE_PREFIX_PATH']
-    prefix = ENV['CMAKE_PREFIX_PATH'].split(":").first
-    prefix = File.join(prefix,"lib","ruby",RbConfig::CONFIG['ruby_version'],RbConfig::CONFIG['arch'],"orocos")
+if prefix = ENV['RUBY_CMAKE_INSTALL_PREFIX']
+    archdir = RbConfig::CONFIG['archdir'].gsub(/\/usr/,"")
+
+    prefix = File.join(prefix,archdir,"orocos")
+    prefix = File.absolute_path(File.join(main_dir, prefix))
 else
     prefix = File.join(main_dir,lib_dir,"orocos")
 end
