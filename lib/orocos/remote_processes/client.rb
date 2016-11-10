1836a9862963bfb11b9dad7ad3aa460cd27cd5a2
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2016-11-10 09:25:41 -0200

    remote_processes: make sure that Client#wait_termination raises EOFError on close

--- lib/orocos/remote_processes/client.rb
+++ lib/orocos/remote_processes/client.rb
@@ -218,6 +218,10 @@ module Orocos
                 reader = select([socket], nil, nil, timeout)
                 return Hash.new if !reader
                 while reader
+                    if socket.eof? # remote closed, probably a crash
+                        raise ComError, "communication to process server closed"
+                    end
+
                     data = socket.read(1)
                     if !data
                         return Hash.new
