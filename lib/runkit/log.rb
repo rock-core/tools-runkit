9a3f61dce935c18d6955e01f2bc54cecf85581f6
Sylvain <sylvain.joyeux@tidewise.io>
2022-10-21 21:45:36 -0300

    chore: remove leftover functionality / deprecated code

--- lib/runkit/log.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require "pocolog"
-require "runkit"
-require "runkit/log/task_context"
-require "runkit/log/log_marker"
-require "runkit/log/replay"
--- lib/runkit/process_server.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Runkit
-    autoload :ProcessClient, "runkit/remote_processes/deprecated"
-    autoload :ProcessServer, "runkit/remote_processes/deprecated"
-    autoload :RemoteProcess, "runkit/remote_processes/deprecated"
-end
